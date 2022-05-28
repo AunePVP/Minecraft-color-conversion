729e005300e49930867d9f9b0728a471cc7c8bea
aunepvp <leo.mhs@icloud.com>
2022-05-27 17:07:17 -0700

    Add call and class

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-//\\/\//\\/\//\\/\//\\/\//\\/\//\\/\//\\/\//\\/\//\\/\//\\/\//\\/\//\\/\//\
+<?php
+//\\/\//\\/\//\\/\//\\/\//\\/\//*\\/\//\\/\//\\/\//\\/\//\\/\//\\/\//\\/\//\
 // The author of this project is AunePVP.   https://github.com/AunePVP   \\
-//\\/\//\\/\//\\/\//\\/\//\\/\//\\/\//\\/\//\\/\//\\/\//\\/\//\\/\//\\/\//\
+//\\/\//\\/\//\\/\//\\/\//\\/\//*\\/\//\\/\//\\/\//\\/\//\\/\//\\/\//\\/\//\
+require_once 'jsonconversion.php';
+use AunePVP\jsonconversion;
+
+$data = json_decode(file_get_contents("https://cdn.muehlhaeusler.online/test.json"));
+$json = "";
+foreach ($data->description->extra as $extra) {
+    $color = $extra->color;
+    $json .= (new jsonconversion)->convertcolor($color);
+    $json .= $extra->text;
+}
+echo $json;
